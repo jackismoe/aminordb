793711d57b1a73a892b45c5b4c48d6068035389d
jackismoe <jacksonmo@me.com>
2020-12-03 17:51:13 -0500

    Fix gemfile

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,8 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-    {:adapter => 'postgresql',
-     :database => 'aminordb',
-     :host => 'aminordb',
-     :port => '5432',
-     :username => 'postgres',
-     :password => 'postgres'})
+db_config = YAML.load_file('config/database.yml')
+ActiveRecord::Base.establish_connection(db_config['production'])
 
 require './app/controllers/application_controller'
 require_all 'app'
